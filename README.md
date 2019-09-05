b997431944a93342cd6b94c0b88c5a90d2017de5
Harmandeep Singh Pannu <iharmanpannu@gmail.com>
2019-09-04 23:24:09 -0700

    Added smooth scrolling

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # jant-transport-llc
- Jant Transport website
+
+Jant Transport website
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/9c40ab52-156b-43f1-be1f-e516b6fbc42f/deploy-status)](https://app.netlify.com/sites/janttransport/deploys)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 document.getElementById('current-year').innerHTML = String(
   new Date().getFullYear()
 )
+
+const about = document.getElementById('about').getBoundingClientRect().top
+const services = document.getElementById('services').getBoundingClientRect().top
+const contact = document.getElementById('contact').getBoundingClientRect().top
+
+document.getElementById('link-home').addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+})
+document.getElementById('link-about').addEventListener('click', () => {
+  window.scrollTo({ top: about, behavior: 'smooth' })
+})
+document.getElementById('link-services').addEventListener('click', () => {
+  window.scrollTo({ top: services, behavior: 'smooth' })
+})
+document.getElementById('link-contact').addEventListener('click', () => {
+  window.scrollTo({ top: contact, behavior: 'smooth' })
+})
